986583b83b847e3d8743ad1d54ba03399a9272ca
Harjinder Salh <harjinder5911@gmail.com>
2023-11-07 12:03:12 +0530

    Fixed Bugs

--- src/app/admin-panel/sidebar/sidebar-routes.config.ts
+++ src/app/admin-panel/sidebar/sidebar-routes.config.ts
@@ -3,20 +3,18 @@ import { RouteInfo } from "./route-info";
 //Sidebar menu Routes and data
 export const BRANCH_ROUTES: RouteInfo[] = [
   { path: '/admin/dashboard', title: 'Dashboard', icon: 'fa fa-tachometer', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [], visibility: true, isAdmin: false },
-  { path: '/admin/weekly-schedule', title: 'Calendar', icon: 'fa fa-calendar', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [], visibility: true, isAdmin: false },
-  { path: '/admin/attendance', title: 'Attendance', icon: 'fa fa-clock-o', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [], visibility: true, isAdmin: false },
   {
-    path: '', title: 'Appointments', icon: 'fa fa-calendar', class: 'has-sub', badge: '', badgeClass: '', isExternalLink: false,
-    submenu: [
-      { path: '/admin/upcoming-appointments', title: 'Upcoming', icon: 'fa fa-list', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [], visibility: true, isAdmin: false },
-      { path: '/admin/in-progress-appointments', title: 'In Progress', icon: 'fa fa-spinner', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [], visibility: true, isAdmin: false },
-      { path: '/admin/historic-appointments', title: 'History', icon: 'fa fa-history', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [], visibility: true, isAdmin: false },
-
-    ], visibility: true, isAdmin: false
-  }, {
     path: '', title: 'Sale', icon: 'fa fa-money', class: 'has-sub', badge: '', badgeClass: '', isExternalLink: false, submenu: [
       { path: '/admin/daily-sale', title: 'Daily Sale', icon: 'fa fa-industry ', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [], visibility: true, isAdmin: true },
-      { path: '/admin/appointments-static', title: 'Service Sales', icon: 'fa fa-list', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [], visibility: true, isAdmin: true },
+      { path: '/admin/appointments-static', title: 'Service Sales', icon: 'fa fa-history', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [], visibility: true, isAdmin: true },
+      { path: '/admin/product-sale-history', title: 'Product Sales', icon: 'fa fa-history', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [], visibility: true, isAdmin: false }
+    ], visibility: true, isAdmin: true
+  },
+  {
+    path: '', title: 'Employees', icon: 'fa fa-users', class: 'has-sub', badge: '', badgeClass: '', isExternalLink: false, submenu: [
+      { path: '/admin/employees', title: 'List', icon: 'ft-users', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [], visibility: true, isAdmin: true },
+      { path: '/admin/leaves', title: 'Leaves', icon: 'fa fa-calendar', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [], visibility: true, isAdmin: false },
+      { path: '/admin/attendance', title: 'Attendance', icon: 'fa fa-clock-o', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [], visibility: true, isAdmin: false },
       // { path: '/admin/loylity-points', title: 'Loylity Point', icon: 'fa fa-gift', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [], visibility: true, isAdmin: true }
     ], visibility: true, isAdmin: true
   },
@@ -28,8 +26,6 @@ export const BRANCH_ROUTES: RouteInfo[] = [
     ], visibility: true, isAdmin: true
   }, {
     path: '', title: 'Products', icon: 'fa fa-houzz', class: 'has-sub', badge: '', badgeClass: '', isExternalLink: false, submenu: [
-      { path: '/admin/product-sale', title: 'New Sale', icon: 'fa fa-tachometer', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [], visibility: true, isAdmin: false },
-      { path: '/admin/product-sale-history', title: 'Sale History', icon: 'fa fa-history', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [], visibility: true, isAdmin: false },
       { path: '/admin/products', title: 'All Products', icon: 'fa fa-list', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [], visibility: true, isAdmin: false },
       { path: '/admin/products-Categoty', title: 'All Categories', icon: 'fa fa-list-alt', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [], visibility: true, isAdmin: false },
     ], visibility: true, isAdmin: false
@@ -41,17 +37,14 @@ export const BRANCH_ROUTES: RouteInfo[] = [
       { path: '/admin/defaultServices', title: 'Load Default', icon: 'fa fa-play', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [], visibility: true, isAdmin: false },
     ], visibility: true, isAdmin: false
   },
-  {
-    path: '', title: 'Admin', icon: 'ft-settings', class: 'has-sub', badge: '', badgeClass: '', isExternalLink: false,
-    submenu: [
-      // { path: '/admin/profile', title: 'My Profile', icon: 'fa fa-user ', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [], visibility: true, isAdmin: true },
-      { path: '/admin/storeList', title: 'Branchs', icon: 'fa fa-industry ', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [], visibility: true, isAdmin: true },
-      { path: '/admin/employees', title: 'Team Members', icon: 'ft-users', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [], visibility: true, isAdmin: true },
-      // { path: '/admin/weekly-schedule', title: 'Weekly Roster', icon: 'fa fa-calendar', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [], visibility: true, isAdmin: false },
-      // { path: '/admin/incentive', title: 'Incentive', icon: 'fa fa-gift', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [], visibility: true, isAdmin: false },
-      { path: '/admin/leaves', title: 'Leaves', icon: 'fa fa-calendar', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [], visibility: true, isAdmin: false },
-    ], visibility: true, isAdmin: true
-  },
+  { path: '/admin/storeList', title: 'Branchs', icon: 'fa fa-industry ', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [], visibility: true, isAdmin: true },
+  // {
+  //   path: '', title: 'Admin', icon: 'ft-settings', class: 'has-sub', badge: '', badgeClass: '', isExternalLink: false,
+  //   submenu: [
+  //     // { path: '/admin/profile', title: 'My Profile', icon: 'fa fa-user ', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [], visibility: true, isAdmin: true },
+  //     // { path: '/admin/incentive', title: 'Incentive', icon: 'fa fa-gift', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [], visibility: true, isAdmin: false },
+  //   ], visibility: true, isAdmin: true
+  // },
   { path: '/admin/settings', title: 'Settings', icon: 'fa ft-settings', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [], visibility: true, isAdmin: false },
 
   // { path: '/admin/marketing', title: 'Marketing', icon: 'fa fa-bullhorn', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [], visibility: true, isAdmin: false },
